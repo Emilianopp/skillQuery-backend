4a00bb5ec8724cbc6fcc7d86a9d51196c1c0707f
Emilianopp <emilianopp550@gmail.com>
2021-11-08 11:13:18 -0500

    updating api

--- api/modules/location.py
+++ api/modules/location.py
@@ -23,9 +23,40 @@ def set_country(country):
 def select_location():
     if not session.get("country") is None:
         country = session['country']
-        regions = db.Scraped_Data.find({"country":country},{"region":-1,"_id":0}).distinct("region")
+        pipe = [{
+                '$lookup': {
+                    'from': "Scraped_Data",
+                    'localField': "url",
+                    'foreignField': "url",
+                    'as': "role_info"
+                }
+            }, {
+                '$match': {
+                    "role_info.region": {
+
+                        "$exists": 'true',
+                        "$ne": 'null'
+                    }
+                }
+
+            }, {
+                '$group': {
+                    '_id': 'null',
+                    'uniqueValues': {
+                        '$addToSet': "$role_info.region"
+                    }
+                }
+            }
+            ]
+
+        query = client.prod.techs.aggregate(pipeline=pipe)   
+        regions = sum(list(query)[0].get("uniqueValues"),[])
+        print(regions)
+        # regions = db.techs.find({"country":country},{"region":-1,"_id":0}).distinct("region")
         out_regions = [re.sub(r"(\w)([A-Z])", r"\1 \2", x) for x in regions]
-        return json.dumps(out_regions)
+        out = sorted([x for x in out_regions if x != "All"])
+        out.insert(0,"All")
+        return json.dumps(out)
     return json.dumps([])
 
 @location.route('/set_region/<region>',methods = ["POST"])
--- api/modules/tech.py
+++ api/modules/tech.py
@@ -13,35 +13,64 @@ tech = Blueprint('tech', __name__)
 def get_tech():
     check_key = lambda x: not session.get(x) is None
     if check_key("country") and check_key("role") and check_key('region'):
-        country = session['country']
-        region = session['region']
-        role = session['role'] 
-        pipe = [{
-            '$lookup':
-            {
-                'from': "Scraped_Data",
-                'localField': "url",
-                'foreignField': "url",
-                'as': "role_info"
-            }
-        },
-            {'$match': {"role_info.country": country ,
-            "role_info.region": region,
-            "role_info.title": role}},
-            {"$group": {"_id": "$found_list"}}
-        ]
-        count = client.prod.Scraped_Data.count( {"country": country ,
-            "region": region,
-            "title": role}) 
-        query = client.prod.techs.aggregate(pipeline=pipe)
-        embedded_list = [x.get("_id") for x in query]
-        tech_list = sum(embedded_list,[])
-        counts = Counter(tech_list)
-        formated_return = [{"label":x,"value":counts.get(x)} for x in counts]
-        sorted_out = sorted(formated_return, key=lambda d: d['value'],reverse= True) 
-        out = {"counts" :sorted_out,"numRoles":count }
+        print('pass')
+        if(session.get('region') == "All"):
+            country = session['country']
+            role = session['role'] 
+            pipe = [{
+                '$lookup':
+                {
+                    'from': "Scraped_Data",
+                    'localField': "url",
+                    'foreignField': "url",
+                    'as': "role_info"
+                }
+            },
+                {'$match': {"role_info.country": country ,
+                "role_info.title": role}},
+                {"$group": {"_id": "$found_list"}}
+            ]
+            count = client.prod.Scraped_Data.count( {"country": country ,
+                "title": role}) 
+            query = client.prod.techs.aggregate(pipeline=pipe)
+            embedded_list = [x.get("_id") for x in query]
+            tech_list = sum(embedded_list,[])
+            print(tech_list)
+            counts = Counter(tech_list)
+            formated_return = [{"label":x,"value":counts.get(x)} for x in counts]
+            sorted_out = sorted(formated_return, key=lambda d: d['value'],reverse= True) 
+            out = {"counts" :sorted_out,"numRoles":count }
+            return json.dumps(out)
 
+        else:
+            country = session['country']
+            region = session['region'].replace(" ","")
+            role = session['role'] 
+            pipe = [{
+                '$lookup':
+                {
+                    'from': "Scraped_Data",
+                    'localField': "url",
+                    'foreignField': "url",
+                    'as': "role_info"
+                }
+            },
+                {'$match': {"role_info.country": country ,
+                "role_info.region": region,
+                "role_info.title": role}},
+                {"$group": {"_id": "$found_list"}}
+            ]
+            count = client.prod.Scraped_Data.count( {"country": country ,
+                "region": region,
+                "title": role}) 
+            query = client.prod.techs.aggregate(pipeline=pipe)
+            embedded_list = [x.get("_id") for x in query]
+            tech_list = sum(embedded_list,[])
 
-        return json.dumps(out)
+            counts = Counter(tech_list)
+            formated_return = [{"label":x,"value":counts.get(x)} for x in counts]
+            sorted_out = sorted(formated_return, key=lambda d: d['value'],reverse= True) 
+            out = {"counts" :sorted_out,"numRoles":count }
+            return json.dumps(out)
     else: 
         return '200'
